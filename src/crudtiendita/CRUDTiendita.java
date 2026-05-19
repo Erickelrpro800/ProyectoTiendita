b75c3696d9d3a8a0a89f7cb9b8e6495e1711377b
MURFTKUKIS <tp240112346@tepatitlan.tecmm.edu.mx>
2026-05-19 13:10:24 -0600

    Prueba

--- src/crudtiendita/CRUDTiendita.java
+++ src/crudtiendita/CRUDTiendita.java
@@ -14,7 +14,8 @@ public class CRUDTiendita {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       frmTiendita fTiendita = new frmTiendita();
+       fTiendita.setVisible(true);
     }
     
 }
